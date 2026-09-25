2ed34382625b54e5efb3003495ac31e9ea76acd3
salman <projeindircomtr@gmail.com>
2026-09-25 15:58:56 +0300

    trafik tam ekran ve geri navigasyon duzeltildi

--- app/src/main/java/com/salman/stoktakip/util/DropdownOgesi.kt
+++
@@ -1,7 +0,0 @@
-package com.salman.stoktakip.util
-
-import java.io.Serializable
-
-data class DropdownOgesi(val id: Int, val ad: String) : Serializable {
-    override fun toString(): String = ad
-}
